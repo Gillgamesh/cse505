1a2efaa129f76d7b86aee1338c915bb419a7f941
Gilvir Gill <gilvirgillgg@gmail.com>
2022-11-19 19:08:09 -0500

    lantern/manim complicated contrived example

--- man_test.py
+++ man_test.py
@@ -1,5 +1,6 @@
 from manim import *
 import manim
+import torch
 
 def sigmoid(z):
     return 1/(1 + np.exp(-z))
@@ -29,8 +30,8 @@ class NonlinearTransformSeries(manim.LinearTransformationScene):
 class ApplyMatrixExample(manim.Scene):
     def construct(self):
         matrix = [
-            [1,-1],
-            [1,1]
+            [1, -1],
+            [1, 1],
         ]
         self.add(
             manim.NumberPlane(),
@@ -43,6 +44,9 @@ class ApplyMatrixExample(manim.Scene):
         input_grid = manim.NumberPlane(
             x_length=2,
             y_length=2,
+            background_line_style={
+                "stroke_color": manim.utils.color.TEAL
+            }
             # x_range=(-1, 5, 1),
             # y_range
         )
@@ -59,8 +63,8 @@ class ApplyMatrixExample(manim.Scene):
         )
         input_grid.prepare_for_nonlinear_transform()
         self.play(
-            manim.ApplyPointwiseFunction(relu, input_grid),
-            manim.ApplyPointwiseFunction(relu, violation_vector),
+            manim.ApplyPointwiseFunction(sigmoid, input_grid),
+            manim.ApplyPointwiseFunction(sigmoid, violation_vector),
         )
         self.play(
             manim.ApplyMatrix(matrix, input_grid),
@@ -70,8 +74,105 @@ class ApplyMatrixExample(manim.Scene):
         # self.play(
         #     manim.Apply(matrix, input_square)
         # )
+
+class BasicSequentialModel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(2,20),
+            torch.nn.ReLU(),
+            torch.nn.Linear(20, 2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(2,2),
+        )
+    def forward(self, x):
+        return self.layers(x)
         
 
+def weird_polar(points):
+    x, y = points[0], points[1]
+    points = np.array([x * np.sqrt(1 - y * y/2), y * np.sqrt(1-x * x/2), 0])
+    points = np.tan(points) / np.tan(1)
+    return points
+
+
+def train_model(X, y, num_epochs=20):
+    X = torch.FloatTensor(X)
+    y = torch.FloatTensor(y)
+    learning_rate = 0.001
+    layers = BasicSequentialModel()
+    layers.train()
+    optimizer = torch.optim.Adam(layers.parameters(), lr=learning_rate)
+    for epoch in range(num_epochs):
+        y_pred = layers(X)
+        loss_func = torch.nn.MSELoss()
+        loss = loss_func(y, y_pred)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        print(f"epoch {epoch}: loss {loss}" )
+    return layers
+
+class NonlinearNoApproxExample(manim.Scene):
+    def construct(self):
+        self.add(
+            manim.NumberPlane(),
+            # manim.Square(side_length=2*1.3, color=manim.utils.color.RED),
+            manim.Circle(color=manim.utils.color.RED)
+            # manim.Vector([1,1]),
+            # manim.Vector([-1,1]),
+        )
+        violation_vector = manim.Vector([1, -3/10], color=manim.utils.color.PINK)
+        # input_square = manim.Square()
+        input_grid = manim.NumberPlane(
+            x_length=2,
+            y_length=2,
+            background_line_style={
+                "stroke_color": manim.utils.color.TEAL
+            }
+            # x_range=(-1, 5, 1),
+            # y_range
+        )
+        input_grid2 = manim.NumberPlane(
+            x_length=2,
+            y_length=2,
+            background_line_style={
+                "stroke_color": manim.utils.color.GREEN
+            }
+        )
+        # self.play(
+        #     # manim.ApplyMatrix(matrix, input_square),
+        #     # manim.ApplyPointwiseFunction(relu, manim.Square()),
+        #     # manim.ApplyMatrix(matrix, manim.Vector([1,0])),
+        #     # manim.ApplyMatrix(matrix, manim.Vector([0,1]))
+        #     )
+        input_grid.prepare_for_nonlinear_transform()
+        model = BasicSequentialModel()
+        model.load_state_dict(torch.load("network.tm"))
+        def run_model(points):
+            with torch.no_grad():
+                x, y = np.array(model(torch.FloatTensor(points)[:2]))
+                return np.array([x,y,0])
+        self.play(
+            manim.ApplyPointwiseFunction(weird_polar, input_grid),
+            manim.ApplyPointwiseFunction(run_model, input_grid2 ),
+            manim.ApplyPointwiseFunction(weird_polar, violation_vector),
+        )
+        
+        # self.play(
+        #     manim.Apply(matrix, input_square)
+        # )
 
 # ApplyMatrixExample().construct()
 # NonlinearTransformSeries()
+
+if __name__=="__main__":
+    num_points = 5000
+    sample_points = np.array(
+        list(zip(np.random.uniform(-1,1,num_points), np.random.uniform(-1,1,num_points)))
+    )
+    mapped_to = np.apply_along_axis(weird_polar, 1, sample_points)[:, :2]
+    print(mapped_to)
+    network = train_model(sample_points, mapped_to, num_epochs=2000)
+    print(network)
+    torch.save(network.state_dict(), "network.tm")
